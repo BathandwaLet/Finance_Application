32b9183e835934ff2ad7972c8cdba3f72106e227
Bathandwa <bmap750@gmail.com>
2026-04-02 16:11:53 +0200

    Initial commit

---
+++ Finance_Application/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Finance_Application.Models;
+
+namespace Finance_Application.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Finance_Application/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Finance_Application.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
