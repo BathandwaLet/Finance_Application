2a14906093c932bff81a855a42c7f60392a846a4
Bathandwa <bmap750@gmail.com>
2026-04-15 15:35:20 +0200

    feat: Implemented Income Interface

---
+++ Finance_Application/Data/Services/IIncomeService.cs
@@ -0,0 +1,12 @@
+using Finance_Application.Models;
+
+namespace Finance_Application.Data.Services;
+
+public interface IIncomeService 
+{
+    Task <IEnumerable<Income>> GetAll();
+    
+    Task Add(Income income);
+
+    IQueryable GetChartData();
+}
