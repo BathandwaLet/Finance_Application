930cf840daa8274deabb4e87aeedc6eb6fdd4ffa
Bathandwa <bmap750@gmail.com>
2026-04-15 15:36:34 +0200

    feat: Implemented Income Controller

---
+++ Finance_Application/Controllers/IncomeController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Finance_Application.Data;
+using Finance_Application.Data.Services;
+using Finance_Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_Application.Controllers;
+
+public class IncomeController : Controller
+{
+    private readonly IIncomeService _incomeService;
+
+    public IncomeController(IIncomeService incomeService)
+    {
+        _incomeService = incomeService;
+    }
+    // GET
+    public async Task <IActionResult> Index()
+    {
+        var income = await _incomeService.GetAll();
+        return View(income);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task <IActionResult> Create(Income income)
+    {
+        if (ModelState.IsValid)
+        {
+            await _incomeService.Add(income);
+            
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult GetChart()
+    {
+        var data = _incomeService.GetChartData();
+        return Json(data);
+    }
+}
